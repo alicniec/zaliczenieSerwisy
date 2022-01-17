f00e3bf753c37f6606e3cf5ad95a941cd1cb29fc
Alicja Niec <alicniec@gmail.com>
2022-01-17 18:36:46 +0100

    zmiana wygladu

--- Sklep/Produkty/admin.py
+++ Sklep/Produkty/admin.py
@@ -2,8 +2,3 @@ from django.contrib import admin
 from .models import Produkty, Gatunki, Kategoria
 
 # Register your models here.
-
-
-admin.site.register(Produkty) #Dodanie produktów do konsoli admina na web stronnie
-admin.site.register(Gatunki)
-admin.site.register(Kategoria)
--- Sklep/Produkty/models.py
+++ Sklep/Produkty/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+#test 12344556
+
 class Gatunki(models.Model):
 
     nazwa = models.CharField(max_length=100)
