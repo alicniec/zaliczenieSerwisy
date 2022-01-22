db35dca13aa5d491e00979b9bcdc8f2436982785
Alicja Niec <alicniec@gmail.com>
2022-01-22 15:58:09 +0100

    test

--- Sklep/Produkty/models.py
+++ Sklep/Produkty/models.py
@@ -6,6 +6,7 @@ from django.db import models
 from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
+#test
 
 class Kategoria(models.Model):
     nazwa = models.CharField(max_length=100)
